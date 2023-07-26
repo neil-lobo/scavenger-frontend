4cc43011c1f3139851b790c31a63e13f1cb6a241
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-07-26 18:42:55 -0400

    add particle background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,57 @@
 	import { setContext } from "svelte";
 	import { writable } from "svelte/store";
 	import Cookies from "js-cookie";
+	import Particles from "svelte-particles";
+	import { loadSlim } from "tsparticles-slim";
 
 	export let data: LayoutData;
 	const tokenStore = writable<string>();
 	setContext("token", tokenStore);
 	let user: User | undefined;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const particlesConfig: any = {
+		particles: {
+			color: {
+				value: "#fdc056"
+			},
+			links: {
+				enable: false
+			},
+			move: {
+				enable: true,
+				speed: 0.25,
+				direction: "top-right"
+			},
+			number: {
+				value: 100
+			},
+			size: {
+				value: {
+					max: 3,
+					min: 1
+				},
+				animation: {
+					enable: true,
+					speed: 1
+				}
+			},
+			opacity: {
+				value: {
+					max: 1,
+					min: 0.5
+				},
+				animation: {
+					enable: true,
+					speed: 0.25
+				}
+			}
+		}
+	};
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const particlesInit = async (engine: any) => {
+		await loadSlim(engine);
+	};
 
 	tokenStore.subscribe((jwt) => {
 		if (jwt === "") {
@@ -49,12 +95,29 @@
 	}
 </script>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/login">Login</a>
-	<a href="/signup">Signup</a>
-	<a href="/logout">Logout</a>
-	{JSON.stringify(user) ?? "no token"}
-</nav>
+<Particles id="tsparticles" class="tsparticles" options={particlesConfig} {particlesInit} />
+<div id="nav">
+	<nav>
+		<a href="/">Home</a>
+		<a href="/login">Login</a>
+		<a href="/signup">Signup</a>
+		<a href="/logout">Logout</a>
+		{JSON.stringify(user) ?? "no token"}
+	</nav>
+</div>
 
-<slot />
+<div id="slot">
+	<slot />
+</div>
+
+<style scoped>
+	#slot,
+	#nav {
+		position: relative;
+		z-index: 1;
+	}
+
+	#nav {
+		background-color: #61002f;
+	}
+</style>
